b74866c1b13e7190e4c0532064389cc389f55e52
Jctech15 <jyctech15@gmail.com>
2022-12-06 15:36:30 -0800

    Nextjs practice

---
+++ pages/games/[id].js
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export default function Game({ game }) {
+  const router = useRouter();
+  // const { id } = router.query;
+
+  return (
+    <>
+      <Head>I am the HEAD</Head>
+      <h1>{game.id} </h1>
+      <img src={game.image} width="500px" />
+    </>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { game: data },
+  };
+}
